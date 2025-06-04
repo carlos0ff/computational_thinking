08f0ac03da27933fe18f3d825f12a1a8d669a492
carlos0ff <carlosiilva66@gmail.com>
2025-06-04 10:51:26 -0300

    Exercício 1: Implementa algoritmo de seleção (Selection Sort)

--- exercicios/algoritmos_ordenacao/ex1_selection_sort.c
+++ exercicios/algoritmos_ordenacao/ex1_selection_sort.c
@@ -1,2 +1,29 @@
+git add ex1_selection_sort.c
+git commit -m "Exercício 1: Implementa algoritmo de seleção (Selection Sort)"
 
+git add ex2_empregado.c
+git commit -m "Exercício 2: Cadastro de empregados com struct e alocação dinâmica"
 
+git add ex3_aeroporto.c
+git commit -m "Exercício 3: Cadastro de aeroportos com ponteiros e alocação dinâmica"
+
+git add ex4_goldbach_700_1100.c
+git commit -m "Exercício 4: Verifica Conjectura de Goldbach de 700 a 1100"
+
+git add ex5_goldbach_ate_n.c
+git commit -m "Exercício 5: Verifica Conjectura de Goldbach até N"
+
+git add ex6_newton_raphson.c
+git commit -m "Exercício 6: Método de Newton-Raphson para raiz quadrada com precisão 0.0001"
+
+git add ex7_quicksort.c
+git commit -m "Exercício 7: Implementa ordenação com QuickSort recursivo"
+
+git add ex8_complexo.c
+git commit -m "Exercício 8: TAD Complexo com operações matemáticas"
+
+git add ex9_bubblesort.c
+git commit -m "Exercício 9: BubbleSort com contagem de ifs e trocas"
+
+git add README.md
+git commit -m "Adiciona README com descrição dos exercícios"
